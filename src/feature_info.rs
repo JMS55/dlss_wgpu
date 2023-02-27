7836798b20e2f1479b953f0abd8a9bedd223c033
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-02-27 12:58:04 -0500

    Fix feature info

--- src/feature_info.rs
+++ src/feature_info.rs
@@ -4,6 +4,7 @@ use std::ffi::{CString, OsStr, OsString};
 use std::ptr;
 use uuid::Uuid;
 
+// TODO: Remove most allocations
 pub fn with_feature_info<F, T>(project_id: Uuid, callback: F) -> T
 where
     F: FnOnce(&NVSDK_NGX_FeatureDiscoveryInfo) -> T,
@@ -26,9 +27,14 @@ where
         os_str_to_wchar(&OsString::from(sdk_path)),
     ];
 
+    let shared_library_path_pointers = shared_library_paths
+        .iter()
+        .map(Vec::as_ptr)
+        .collect::<Vec<_>>();
+
     let feature_info_common = NVSDK_NGX_FeatureCommonInfo {
         PathListInfo: NVSDK_NGX_PathListInfo {
-            Path: shared_library_paths.as_ptr().cast(),
+            Path: shared_library_path_pointers.as_ptr(),
             Length: shared_library_paths.len() as u32,
         },
         InternalData: ptr::null_mut(),
@@ -61,17 +67,18 @@ where
 }
 
 #[cfg(target_os = "windows")]
-fn os_str_to_wchar(s: &OsStr) -> Box<[wchar_t]> {
+fn os_str_to_wchar(s: &OsStr) -> Vec<wchar_t> {
     use std::os::windows::ffi::OsStrExt;
 
-    s.encode_wide().map(|c| c as wchar_t).collect()
+    s.encode_wide().chain([0]).map(|c| c as wchar_t).collect()
 }
 
 #[cfg(not(target_os = "windows"))]
-fn os_str_to_wchar(s: &OsStr) -> Box<[wchar_t]> {
+fn os_str_to_wchar(s: &OsStr) -> Vec<wchar_t> {
     s.to_str()
         .unwrap_or("")
         .chars()
+        .chain([0 as char])
         .map(|c| c as wchar_t)
         .collect()
 }
